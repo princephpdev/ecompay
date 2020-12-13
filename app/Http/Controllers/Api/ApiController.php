332f64a22a8fef1920c9af420ca1a38af8e77d1d
Prince K <princephpdev@gmail.com>
2020-12-13 13:36:13 +0530

    Added Api Auth with login, Registration using sanctum

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
@@ -11,9 +14,60 @@ class ApiController extends Controller
         return "Awesome Api";
     }
 
-    public function createToken(Request $request) {
-        $token = $request->user()->createToken($request->token_name);
-    
-        return ['token' => $token->plainTextToken];
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+            // 'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.',
+            ],404);
+        }
+        return $user->createToken('gofoofa_user')->plainTextToken;
     }
+
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Logged Out Successfully',
+        ],200);
+    }
+
+    public function getUser(){
+        return Auth::user();
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name'=> 'string|required|max:50',
+            'email'=> 'email|required|unique:users',
+            'password'=> 'required|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' =>$request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+            // Only Message if registration is successfull
+        return response()->json([
+            'message' => 'User Created Successfully',
+        ],200);
+
+        // Login with registration
+        // return $user->createToken('gofoofa_user')->plainTextToken;
+
+    }
+
+    // public function createToken(Request $request) {
+    //     $token = $request->user()->createToken($request->token_name);
+    
+    //     return ['token' => $token->plainTextToken];
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,25 @@ use App\Http\Controllers\Api\ApiController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('auth/v1')->group(function () {
+    
+    Route::post('login', [ApiController::class, 'login']);
+    Route::post('register', [ApiController::class, 'store']);
+    
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('user', [ApiController::class, 'getUser']);
+        Route::delete('logout', [ApiController::class, 'logout']);
+    });
 
-Route::get('token', [ApiController::class, 'index']);
-Route::post('/tokens/create', [ApiController::class, 'createToken']);
+    // Route::get('/', function(){
+    //     return response()->json([
+    //         'message'=>'hello from api',
+    //     ],403);
+    // });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // Route::post('/tokens/create', [ApiController::class, 'createToken']);
+    
+    // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    //     return $request->user();
+    // });     
 });
